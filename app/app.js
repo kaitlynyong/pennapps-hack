11d3c93ca294d3c808b71545f58761b1f5b58d9c
Edward Kim <eddotman@gmail.com>
2015-09-05 11:50:36 -0400

    heroku

--- app/app.js
+++ app/app.js
@@ -1,10 +1,5 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
-
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
