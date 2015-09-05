92f47561ef5987d5c8f1844cda679bad4d8c60c5
Edward Kim <eddotman@gmail.com>
2015-09-04 22:50:36 -0400

    basic text parsing working

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from spacy import en
+#from spacy.en import English
 from pdfs import (request_form)
 
+#nlp = English()
 app = Flask(__name__)
+#app.config['DEBUG'] = True
 
 @app.route('/<form>')
 def app_request_form(form):
   return request_form(form)
 
 if __name__ == '__main__':
-  #nlp = en.English()
   app.run()
--- pdfs.py
+++ pdfs.py
@@ -1,2 +1,17 @@
+from spacy.en import English
+nlp = English()
+
 def request_form(form):
-  return 'Hello World: ' + str(form)
+  test_sentence = unicode("Enter your TIN in the appropriate box. The TIN provided must match the name given on line 1 to avoid backup withholding. For individuals, this is generally your social security number (SSN). However, for a resident alien, sole proprietor, or disregarded entity, see the Part I instructions on page 3. For other entities, it is your employer identification number (EIN). If you do not have a number, see How to get a TIN on page 3.")
+  tokens = nlp(test_sentence)
+  
+  #this check should be last
+  fill_verbs = set(['enter', 'fill', 'provide', 'list'])
+  verbs = [t.orth_ for t in tokens if (t.pos_ == 'VERB' and str(t.orth_).lower() in fill_verbs)]
+  
+  actions = []
+  for token in tokens:
+    if token.pos_ == 'NOUN' and token.head.orth_ in verbs:
+      actions.append(str(token.head.orth_) + " " + str(token.orth_))
+
+  return str(form) + " " + str(actions)
