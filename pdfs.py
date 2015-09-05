d449a87554f9442e7050ab5d675aa7c8fb1ec06f
Edward Kim <eddotman@gmail.com>
2015-09-05 00:00:25 -0400

    json responses

--- pdfs.py
+++ pdfs.py
@@ -1,6 +1,6 @@
 from spacy.en import English
 from slate import PDF
-import json
+from flask import jsonify
 
 nlp = English()
 
@@ -11,6 +11,18 @@ def request_form(form):
 
   if pdf is None: return 1
 
+  ret_doc = {
+    "instructions": "",
+    "inputs": [
+      # {
+      #   "type": "checkbox|text|textarea|radio|date|address|signature",
+      #   "name": "text",
+      #   "value": "text | boolean | number",
+      #   "coordinates": ["float", "float"]
+      # }
+    ]
+  }
+
   u_pdf = unicode('')
   for page in pdf:
     u_pdf += str(page).decode('utf-8')
@@ -19,12 +31,20 @@ def request_form(form):
   
   #this check should be last
   fill_verbs = set(['enter', 'fill', 'provide', 'list'])
-  verbs = [t.orth_ for t in tokens if (t.pos_ == 'VERB' and str(t.orth_).lower() in fill_verbs)]
+  verbs = [t for t in tokens if (t.pos_ == 'VERB' and str(t.orth_).lower() in fill_verbs)]
   
   actions = []
-  for token in tokens:
-    if token.pos_ == 'NOUN' and token.head.orth_ in verbs:
-      actions.append(unicode(token.head.orth_) + u' ' + unicode(token.orth_))
+  for verb in verbs:
+    action = ' '.join([t.orth_ for t in verb.subtree])
+    actions.append(action)
 
+  for action in actions:
+    new_input = {
+      "type": "text",
+      "name": action,
+      "value": None,
+      "coordinates": [None, None]
+    }
+    ret_doc['inputs'].append(new_input)
 
-  return json.dumps(actions)
+  return jsonify(ret_doc)
