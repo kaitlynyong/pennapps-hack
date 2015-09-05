eff7538062597a15e0dfd14afd2682a7598c61aa
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2015-09-04 22:33:48 -0700

    Landing page added

--- app/app.js
+++ app/app.js
@@ -5,9 +5,10 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.landing',
   'myApp.version',
   'ngFileUpload'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/landing'});
 }]);
